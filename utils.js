e3ef6a22332f94c270c4c2839b64aae5393c21d9
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-04-10 11:43:07 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ const sendFile = async (item, ctx) => {
       await ctx.replyWithDocument(item);
     } catch (e) {
       ctx.replyWithMarkdown(
-        `⚠️ ${e.message}\n\n👉 Try manually downloading from [here](${item})\n\n👉 *Maybe This File Is Too Large Or Cannot Accessible From Terabox*`,
+        `⚠️ ${e.message}\n\n👉 Try manually downloading from [here](${item})\n\n👉 *Maybe This File Is Too Large Or Cannot Accessible From Terabox \n\n You can download the file by clicking on here*`,
       );
     }
   }
